8fb775757d10b542367ac615b4ca0819b4264845
Soja <soja78@gmail.com>
2018-02-07 15:00:05 +0530

    Arithematic

---
+++ arithmetic.sh
@@ -0,0 +1,16 @@
+echo "enter two numbers"
+read a b
+ val=`expr $a + $b`
+echo "a + b : $val"
+
+val=`expr $a - $b`
+echo "a - b : $val"
+
+val=`expr $a \* $b`
+echo "a * b : $val"
+
+val=`expr $b / $a`
+echo "b / a : $val"
+
+val=`expr $b % $a`
+echo "b % a : $val"
